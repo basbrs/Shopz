cb41303c6f41a5f6b36282b7d7a3d3efaf64a389
basbrs <b.bauers@live.de>
2021-03-04 21:20:46 +0100

    Rewrote minor code passages to compile using Java8, created Java8 .jar

--- src/de/babrs/shopz/listerners/InventoryClickListener.java
+++ src/de/babrs/shopz/listerners/InventoryClickListener.java
@@ -1,9 +1,10 @@
 package de.babrs.shopz.listerners;
 
+import de.babrs.shopz.ShopzPlugin;
 import de.babrs.shopz.inventories.InventoriesSingleton;
 import de.babrs.shopz.inventories.SetupInventory;
 import de.babrs.shopz.inventories.ShoppingInventory;
-import de.babrs.shopz.ShopzPlugin;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -27,22 +28,29 @@ public class InventoryClickListener implements Listener{
                 else if(event.getClick() == ClickType.SHIFT_LEFT) transactions = 5;
 
                 ShoppingInventory shop = InventoriesSingleton.getShopWithInventory(event.getInventory());
-                for(int i = 0; i < transactions; i++)
-                    switch(event.getClickedInventory().getItem(event.getRawSlot()).getType()){
-                        case STRUCTURE_VOID    -> shop.trade((Player) event.getWhoClicked(), true);
-                        case BARRIER           -> shop.trade((Player) event.getWhoClicked(), false);
-                    }
+                for(int i = 0; i < transactions; i++){
+                    Material item = event.getClickedInventory().getItem(event.getRawSlot()).getType();
+                    if(item == Material.STRUCTURE_VOID)
+                        shop.trade((Player) event.getWhoClicked(), true);
+                    else if(item == Material.BARRIER)
+                        shop.trade((Player) event.getWhoClicked(), false);
+                }
             }else{
                 SetupInventory setup = InventoriesSingleton.getSetupFrom((Player) event.getWhoClicked());
                 if(setup != null && event.getRawSlot() < 5 && event.getRawSlot() >= 0){
-                    if(event.getClick() == ClickType.LEFT)
-                        switch(event.getRawSlot()){
-                            case 0 -> setup.changeSellPrice(-ShopzPlugin.getPluginConfig().getInt("price_step_size"));
-                            case 1 -> setup.changeSellPrice(ShopzPlugin.getPluginConfig().getInt("price_step_size"));
-                            case 2 -> setup.changeGood(event.getWhoClicked().getItemOnCursor());
-                            case 3 -> setup.changeBuyPrice(-ShopzPlugin.getPluginConfig().getInt("price_step_size"));
-                            case 4 -> setup.changeBuyPrice(ShopzPlugin.getPluginConfig().getInt("price_step_size"));
-                        }
+                    if(event.getClick() == ClickType.LEFT){
+                        int slot = event.getRawSlot();
+                        if(slot == 0)
+                            setup.changeSellPrice(-ShopzPlugin.getPluginConfig().getInt("price_step_size"));
+                        else if(slot == 1)
+                            setup.changeSellPrice(ShopzPlugin.getPluginConfig().getInt("price_step_size"));
+                        else if(slot == 2)
+                            setup.changeGood(event.getWhoClicked().getItemOnCursor());
+                        else if(slot == 3)
+                            setup.changeBuyPrice(-ShopzPlugin.getPluginConfig().getInt("price_step_size"));
+                        else if(slot == 4)
+                            setup.changeBuyPrice(ShopzPlugin.getPluginConfig().getInt("price_step_size"));
+                    }
                     event.setCancelled(true);
                 }
             }
